4d81752d0495dc93ab643d3445e7a506dc563088
Jet Yang <1239143744@qq.com>
2018-06-19 12:02:37 +0800

    Initial commit

---
+++ yang.c
@@ -0,0 +1,8 @@
+#include"stdio.h"
+
+int main()
+{
+	int c = 3;
+	printf("%d\n",c);
+	return 0;
+}
---
+++ yang.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int c = 2;
+	cout<<c<<endl;
+
+	return 0;
+}
